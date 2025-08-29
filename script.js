c62e89ed9bc638c386c617cc356e00773acf1728
Sherif.219960 <sherifanwar76@gmail.com>
2025-08-29 12:44:18 +0200

    make function to create new post and make function to scroll top for window

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const addPostBtn = document.querySelector(".addPost");
 if (localStorage.length != 0) {
   toggleLoginSection("toggle", "d-none");
+  addPostBtn.classList.remove("d-none");
 } else {
   toggleLogoutSection("toggle", "d-none");
 }
@@ -8,64 +10,56 @@ let token;
 token = localStorage.getItem("token");
 const login_logout = document.querySelector(".login-logout");
 
+// make function to show posts
 async function showPosts(url) {
   const post_section = document.querySelector(".post-section");
-
-  const response = await axios
-    .get(`${url}/posts?limit=2`)
-    .then((response) => {
-      post_section.innerHTML = "";
-      const posts = response.data.data;
-
-      for (const post of posts) {
-        let { created_at, title, image, comments_count } = post;
-        let { username, profile_image } = post.author;
-        if (typeof image === "object") {
-          image = profile_image;
-        } else {
-          image = image;
-        }
-        post_section.innerHTML += `
+  post_section.innerHTML = "";
+  try {
+    const response = await axios.get(`${url}/posts?limit=5`);
+    const { data } = response.data;
+    console.log(data);
+    for (const post of data) {
+      let { created_at, title, image, comments_count, body } = post;
+      let { username, profile_image } = post.author;
+      if (typeof image === "object") {
+        image = profile_image;
+      } else {
+        image = image;
+      }
+      post_section.innerHTML += `
               <div class="col-md-9 ">
                 <div class="card shadow ">
                     <div class="border">
                         <img src="${image}" alt="user" class="rounded-circle m-2" width="40" height="40">
                         <span class="fw-bold">${username}</span>
                     </div>
-                    
+
                     <img src=${image} class="w-100" alt="user">
                     <span class="small m-1">2 ${created_at}</span>
                     <div class="card-body">
 
                         <h5 class="card-title">${title}</h5>
                         <p class="card-text">
-                            This card has supporting text below as a natural lead-in to additional content.
+                            ${body}
                         </p>
                         <p class="card-text border-top">
 
                             <small class="text-body-secondary ">
-                                <i class="fa-solid fa-pen text-primary"></i> <span>comments(${comments_count})</span>
+                                <i class="fa-solid fa-pen text-primary"></i> <span>comments(${
+                                  comments_count == 0
+                                    ? Math.floor(Math.random() * 10) + 1
+                                    : comments_count
+                                })</span>
                             </small>
                         </p>
                     </div>
                 </div>
             </div>
             `;
-      }
-    })
-    .catch((error) => {
-      if (error.response) {
-        // Server responded with a status other than 2xx
-        console.error("Error Response:", error.response.data);
-        console.error("Status Code:", error.response.status);
-      } else if (error.request) {
-        // Request was made but no response received
-        console.error("No Response:", error.request);
-      } else {
-        // Something else happened
-        console.error("Error Message:", error.message);
-      }
-    });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 showPosts(url);
@@ -73,6 +67,7 @@ showPosts(url);
 function login(url) {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
+  const user_info_name = document.querySelector(".user-info-name");
 
   const params = {
     username: username,
@@ -84,14 +79,19 @@ function login(url) {
     .then((response) => {
       token = response.data.token;
       const user = response.data.user;
+      user_info_name.innerHTML = user.name;
       localStorage.setItem("token", token);
       localStorage.setItem("user", JSON.stringify(user));
       successMessage(`Hi ${user.username} Login successes`, "success");
+      localStorage.length != 0
+        ? addPostBtn.classList.remove("d-none")
+        : addPostBtn.classList.add("d-none");
       toggleLogoutSection("remove", "d-none");
       toggleLoginSection("add", "d-none");
     })
     .catch((error) => {
-      console.log(error);
+      const message = error.response.data.message;
+      successMessage(`❌ ${message}`, "danger");
     });
 }
 
@@ -101,6 +101,7 @@ function myForm(e) {
   const modalEl = document.getElementById("loginModal");
   const Modal = bootstrap.Modal.getInstance(modalEl);
   login(url);
+
   document.activeElement.blur();
   Modal.hide();
 }
@@ -121,7 +122,7 @@ function successMessage(message, type) {
         document.querySelector(".alert")
       );
       alert.close();
-    }, 1000);
+    }, 1500);
   };
 
   appendAlert(message, type);
@@ -140,6 +141,7 @@ function logOut() {
   localStorage.clear();
   toggleLogoutSection("add", "d-none");
   toggleLoginSection("remove", "d-none");
+  addPostBtn.classList.add("d-none");
   successMessage("! Note You make Logout ....", "danger");
 }
 
@@ -199,14 +201,66 @@ function myFormSignUp(e) {
     axios
       .post(`${url}/register`, params)
       .then((res) => {
-        console.log("✅ User registered:", res.data);
+        console.log("✅ User registered:", res.data.token, res.data.user);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("user", JSON.stringify(res.data.user));
         successMessage("✅ User registered successfully", "success");
         toggleLogoutSection("remove", "d-none");
         toggleLoginSection("add", "d-none");
       })
       .catch((error) => {
-        console.log(error.response.data.message);
+        const message = error.response.data.message;
+        successMessage(`❌ ${message}`, "danger");
       });
   }
   signupForm();
 }
+
+function createPost() {
+  const title = document.getElementById("postTitle").value;
+  const content = document.getElementById("postContent").value;
+  const image = document.getElementById("postImage").files[0];
+
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("body", content);
+  formData.append("image", image);
+
+  axios
+    .post(`https://tarmeezacademy.com/api/v1/posts`, formData, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`, // Example
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      showPosts(url);
+
+      const modalEl = document.getElementById("postModal");
+      const Modal = bootstrap.Modal.getInstance(modalEl);
+      document.activeElement.blur();
+      Modal.hide();
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
+    });
+}
+
+// make scroll top function
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  console.log("scrollToTop");
+}
+
+window.addEventListener("scroll", () => {
+  const scrollBtnTop = document.querySelector(".scroll-to-top");
+  if (window.scrollY >= 100) {
+    scrollBtnTop.classList.remove("d-none");
+  } else {
+    scrollBtnTop.classList.add("d-none");
+  }
+});
